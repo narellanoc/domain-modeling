4f9d81742ad6eb385a7fc2cf4af92eb0f8fa80bc
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-04-15 21:26:53 +0000

    clase 4/15

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,38 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
-
+DROP TABLE IF EXISTS salesperson;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
 -- CREATE TABLES
+CREATE TABLE salesperson(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE companies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE contacts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number INTEGER,
+    company_id INTEGER,
+    title TEXT
+);
+
+CREATE TABLE activities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    salesperson_id INTEGER,
+    contact_id INTEGER,
+    type TEXT,
+    date TEXT,
+    description TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,43 @@
+-- Drops existing tables, so we start fresh with each
+-- run of this script
+-- e.g. DROP TABLE IF EXISTS ______;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS follower;
+
+-- CREATE TABLES
+-- ENTITIES
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    name TEXT,
+    location TEXT
+);
+CREATE TABLE posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    description TEXT,
+    created_at TEXT
+);
+--RELATIONS
+CREATE TABLE likes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    post_id INTEGER
+);
+CREATE TABLE comments(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    post_id INTEGER,
+    body TEXT
+);
+CREATE TABLE follower(
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     follower_user_id INTEGER,
+     followed_user_id INTEGER 
+);
 
